145ea7cf4c6bbd091a860a92bcc9e49a66c7b41a
VKStriicK <loicbgrt@gmail.com>
2024-06-18 13:11:36 +0000

    I think my variable is too long and Betty doesn't like it...

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l_digit = n % 10;
-	if (last_digit > 5)
+	if (l_digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
 	}
