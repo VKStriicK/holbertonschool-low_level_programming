c45174e3f3e28dbc9a8bb6a3439c3e3703e9556d
VKStriicK <loicbgrt@gmail.com>
2024-06-18 10:14:53 +0000

    My code isn't Betty friendly...

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Main function
- * 
+ *
  * Return: Always 0 (Success)
  */
 
@@ -14,12 +14,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0){
+	if (n>0)
+	{
 		printf("%d is positive\n", n);
-	} else if (n<0){
+	} else if (n<0)
+	{
 		printf("%d is negative\n", n);
 	}
-	else {
+	else
+	{
 		printf("%d is zero\n", n);
 	}
 	return (0);
