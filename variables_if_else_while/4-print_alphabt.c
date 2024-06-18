3960549d2c7c7c8b1bf8d94a65f62018f1d63704
VKStriicK <loicbgrt@gmail.com>
2024-06-18 13:51:29 +0000

    Printing alphabet without 'q' and 'e'

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'q' || alphabet == 'e')
+		{
+			alphabet++;
+		}
+		else
+		{
+			putchar(alphabet);
+			alphabet++;
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
