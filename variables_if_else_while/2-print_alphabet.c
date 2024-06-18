7bd241a99eb322e761c6a38cfbb7665af1649f41
VKStriicK <loicbgrt@gmail.com>
2024-06-18 13:18:12 +0000

    This is a test

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (a <= z)
+	{
+		putchar("%d\n", alphabet)
+		alphabet ++
+	}
+	return (0);
+}
