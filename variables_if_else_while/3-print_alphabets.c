06297279a6026cc06deee605152dbae13f864def
VKStriicK <loicbgrt@gmail.com>
2024-06-18 13:34:09 +0000

    Printing the alphabet and the ALPHABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char alphabet = 'a';
+	char ALPHABET = 'A';
+
+        while (alphabet <= 'z')
+        {
+                putchar(alphabet);
+                alphabet++;
+        }
+	while (ALPHABET <= 'Z')
+	{
+		putchar(ALPHABET);
+		ALPHABET++;
+	}
+        putchar('\n');
+        return (0);
+}
