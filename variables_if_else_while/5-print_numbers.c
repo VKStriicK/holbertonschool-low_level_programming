c00c80bd9c004fc4b6e2fb34b7c1844549b67947
VKStriicK <loicbgrt@gmail.com>
2024-06-19 12:47:58 +0000

    Printing all the numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0 ;  number < 10 ; number++)
+	{
+		printf("%d", number);
+	}
+	putchar("/n");
+	return (0);
+}
