3f612b994d39cdf4accd2e32f10c1c98b47aa69d
VKStriicK <loicbgrt@gmail.com>
2024-06-19 13:42:56 +0000

    Printing numbers and letters

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+	int letter = 0;
+	for (number == 48; number <= 58; number++)
+	{
+		putchar(number);
+	}
+	for (letter == 'a'; letter <= 'f'; letter ++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
