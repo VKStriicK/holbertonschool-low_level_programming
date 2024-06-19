1aa3bf22f7f33cbc694bd67d3163ef819d1abcaa
VKStriicK <loicbgrt@gmail.com>
2024-06-19 13:06:34 +0000

    Printing all the numbers, without printf

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Main funciton
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
