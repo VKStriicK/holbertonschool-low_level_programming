74af5d7db9d3e40e643a94faeb9ffa287752ccfb
VKStriicK <loicbgrt@gmail.com>
2024-06-23 13:00:30 +0000

    Adding the result of 2 int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Main function
+ * @n: The first int
+ * @c: The second int
+ *
+ *
+ * Return: The result of the 2 int
+ */
+
+int add(int n, int c)
+{
+	int c;
+	int n;
+
+	return (c + n);
+}
