ecb3a5fde660cc8d53b84b375050320100df7692
VKStriicK <loicbgrt@gmail.com>
2024-06-23 12:42:54 +0000

    Printing the 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+include "main.h"
+
+/**
+ * times_table - Main function
+ *
+ * Return: The 9 times table
+ */
+
+void times_table(void)
+{
+	int i;
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (i = 0; i <= 10; i++)
+		{
+			_putchar(i * n);
+			_putchar (',');
+			_putchar(' ');
+			-putchar(' ');
+		}
+	}
+}
