1349715fc9477d842d53f4df207f20cc69a0d0bd
VKStriicK <loicbgrt@gmail.com>
2024-06-23 12:30:11 +0000

    Printing every minute of a day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Main function
+ *
+ * Return : All the minutes of a day
+ */
+
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+	for (h = 0; h < 24; h ++)
+	{
+		for (m = 0; m < 60; m ++)
+		{
+			_putchar(h);
+			_putchar(':');
+			_putchar(m);
+		}
+	}
+}
