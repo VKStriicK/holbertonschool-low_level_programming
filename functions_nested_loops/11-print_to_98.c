7db20bb48a40d3cc72726606a156694591d41d48
VKStriicK <loicbgrt@gmail.com>
2024-06-23 13:11:14 +0000

    Printing all natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_to_98 - main function
+ * @n: Number to start printing
+ *
+ * Return: All natural numbers from n to 98
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n <= 98; n + -)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n >= 98, n++)
+		{
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		_putchar (n);
+	}
+}
