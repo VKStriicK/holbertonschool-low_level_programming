63bd63ac0f37c0159a358eef0c4b35207f9785f6
VKStriicK <loicbgrt@gmail.com>
2024-06-23 12:02:56 +0000

    Returning the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Main function
+ *@n: The number
+ *
+ * Return: The absolute value of an integer
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
