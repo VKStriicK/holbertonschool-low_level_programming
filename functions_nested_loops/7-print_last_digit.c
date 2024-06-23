efd363f121e57ce3ac73e7a26724287f35a01aa2
VKStriicK <loicbgrt@gmail.com>
2024-06-23 12:22:46 +0000

    Printing the last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,5 +9,18 @@
 
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = n % 10 * -1;
+		_putchar(last_digit);
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit);
+		return (last_digit);
+	}
 }
