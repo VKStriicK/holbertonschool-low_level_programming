42d5f92880881cc2233ae7ff0a29021a4027c9b8
VKStriicK <loicbgrt@gmail.com>
2024-06-24 09:12:55 +0000

    Multiplying two ints

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Main function
+ *
+ * Return: All the numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n);
+}
