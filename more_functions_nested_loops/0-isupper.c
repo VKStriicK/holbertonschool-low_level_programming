d74c4e698e5d670f3121fe12697f755d44a06a07
VKStriicK <loicbgrt@gmail.com>
2024-06-24 08:29:18 +0000

    Checking for uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * _isupper - Main function
+ * @c: The number to check
+ *
+ * Return: 1 if c is uppercase, 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+}
