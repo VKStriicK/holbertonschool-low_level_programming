2edd6ab00fea5c350db127968559eb9fb941eb6c
VKStriicK <loicbgrt@gmail.com>
2024-06-25 12:32:45 +0000

    Printing a line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * print_line - Main function
+ * @n: Number of times the character '_' is printed
+ *
+ * Return: The line
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+	for (i = o; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
