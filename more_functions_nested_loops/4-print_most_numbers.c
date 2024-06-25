be46a1085908f4789514763fa1fc0960fa3d30d3
VKStriicK <loicbgrt@gmail.com>
2024-06-25 09:00:03 +0000

    Printing numbers, except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#inlcude "main.h"
+
+/**
+ * print_most_numbers - Main function
+ *
+ * Return: All numbers, except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2) && (n != 4)
+		{
+			_putchar(n + '0');
+		}
+	}
+	_putchar('\n');
+}
