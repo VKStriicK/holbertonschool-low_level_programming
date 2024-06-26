5d79265b2294a13649a05ad50d0242bb895ba071
VKStriicK <loicbgrt@gmail.com>
2024-06-26 09:26:19 +0000

    Printing a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - Main function
+ * @size: The size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j <= size; j++)
+		{
+			_putchar("#");
+		}
+
+	}
+}
