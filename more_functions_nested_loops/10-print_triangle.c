4371b4a9fdf8cf886da43321ed9ae0ca66662719
VKStriicK <loicbgrt@gmail.com>
2024-06-26 09:27:33 +0000

    Printing a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ void print_triangle(int size)
 		_putchar('\n');
 	}
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= size; j++)
+		for (j = 0; j < size; j++)
 		{
 			_putchar(35);
 		}
