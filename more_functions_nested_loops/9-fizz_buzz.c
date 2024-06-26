922d7b8a51dd338f611c3c8941af3c3765d94f55
VKStriicK <loicbgrt@gmail.com>
2024-06-26 09:18:22 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int mult3 = 0;
+	int mult5 = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		mult3 = i % 3;
+		mult5 = i % 5;
+
+		if ((mult3 == 0) && (mult5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (mult3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (mult5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
