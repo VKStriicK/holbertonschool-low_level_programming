1ee502e9d5f5f6295dfd82b1af39b358b69bddde
VKStriicK <loicbgrt@gmail.com>
2024-06-26 08:36:05 +0000

    Printing a line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,19 +10,10 @@
 void print_line(int n)
 {
 	int i = 0;
-	for (i = 0; i <= n; i++)
+
+	for (i = 0; i < n; i++)
 	{
-		if (n != 0)
-		{
-			_putchar('_');
-		}
-	}
-	for (i = 0; i >= n; i++)
-	{
-		if (n != 0)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
