cc08455b2fe693f511dad2684388d5980c34b070
VKStriicK <loicbgrt@gmail.com>
2024-06-28 10:00:07 +0000

    Copying a string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - Main function
+ *
+ * @dest: The destination variable to copy
+ * @src: The variable to copy
+ *
+ * Return : The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int lenght = 0;
+
+	while (src[lenght])
+	{
+		dest[lenght] = src[lenght];
+		lenght++;
+	}
+
+	return (dest);
+}
