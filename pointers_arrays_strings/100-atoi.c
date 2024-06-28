8dffc48068cc2d59044f43336db14c6f6f97e0a4
VKStriicK <loicbgrt@gmail.com>
2024-06-28 10:37:14 +0000

    Converting a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,15 +10,5 @@
 
 int _atoi(char *s)
 {
-	int lenght = 0;
-	int n = 0;
 
-	for (n = 0; n < s[lenght]; lenght++)
-	{
-		if ((n <= 57) && (n >= 48))
-		{
-			_putchar(n);
-		}
-	}
-	_putchar('\n');
 }
