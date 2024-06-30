69a13da47f2eeaf0cfaacd06f4542316156da4a8
VKStriicK <loicbgrt@gmail.com>
2024-06-30 11:52:41 +0000

    Encoding a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - Main function
+ * @s: The string to encode
+ *
+ * Return: The string after modifications
+ */
+
+char *leet(char *s)
+{
+	char letters[] = {'a', 'e', 'o', 't', 'l'};
+	char numbers[] = {'4', '3', '0', '7', '1'};
+	char lenght = 0;
+	char i = 0;
+
+	while (s[lenght] != '\0')
+	{
+		for (i = 0; numbers[i] != '\0'; i++)
+		{
+			if (s[i] == letters[i])
+			{
+				s[i] = numbers[i];
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
